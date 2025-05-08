232862bf919c7363c87e7c6cec35fbbf8d5286a0
garytayl <132408374+garytayl@users.noreply.github.com>
2025-05-08 16:36:01 -0400

    Update models.py

--- models.py
+++ models.py
@@ -9,3 +9,11 @@ class User(Base):
     name = Column(String)  # ← This line is probably missing
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_email = Column(String, index=True)
+    filename = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
